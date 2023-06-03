40feb9acf43b9b2d54319fccd86b5edc42f6e812
renee <s.n.ruschell@gmail.com>
2023-06-03 14:03:04 +0800

    add objEnhance.js

---
+++ objEnhance.js
@@ -0,0 +1,37 @@
+//Same keys and values
+
+function createInstructor(firstName, lastName){
+    return{
+      firstName,
+      lastName
+    };
+  }
+
+  //Computed Property Names
+  const favoriteNumber = 42;
+  const instructor = {
+    firstName: "Colt",
+    [favoriteNumber]: "That is my favorite!"
+  };
+
+
+  //Object Methods
+  const instructor2 = {
+    firstName: "Colt",
+    sayHi(){
+      return "Hi!";
+    },
+    sayBye(){
+      return this.firstName + " says bye!";
+    }
+  };
+
+  //createAnimal function
+  function createAnimal(species, verb, noise){
+    return{
+        species,
+        [verb](){
+            return noise;
+        }
+    };
+  }
