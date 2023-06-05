395477856bd132deb65ea0b82578c2bcb39e8a1f
renee <s.n.ruschell@gmail.com>
2023-06-05 10:16:25 +0800

    add destructuring exercises

--- objEnhance.js
+++ objEnhance.js
@@ -1,3 +1,5 @@
+
+//OBJECT ENHANCEMENT
 //Same keys and values
 
 function createInstructor(firstName, lastName){
@@ -35,3 +37,73 @@ function createInstructor(firstName, lastName){
         }
     };
   }
+
+//DESTRUCTURING
+//Object Destructuring 1
+let facts = {numPlanets: 8, yearNeptuneDiscovered: 1846};
+let {numPlanets, yearNeptuneDiscovered} = facts;
+
+console.log(numPlanets); // 8
+console.log(yearNeptuneDiscovered); // 1846
+
+//Object Destructuring 2
+let planetFacts = {
+  numPlanets: 8,
+  yearNeptuneDiscovered: 1846,
+  yearMarsDiscovered: 1659
+};
+
+let {numPlanets, ...discoveryYears} = planetFacts;
+
+console.log(discoveryYears); // 1846, 1659
+
+//Object Destructuring 3
+function getUserData({firstName, favoriteColor="green"}){
+  return `Your name is ${firstName} and you like ${favoriteColor}`;
+}
+
+getUserData({firstName: "Alejandro", favoriteColor: "purple"}) // Your first name is Alejandro and you like purple
+getUserData({firstName: "Melissa"}) // Your first name is Melissa and you like green
+getUserData({}) // Your first name is undefined and you like green
+
+//Array Destructuring 1
+let [first, second, third] = ["Maya", "Marisa", "Chi"];
+
+console.log(first); // Maya
+console.log(second); // Marisa
+console.log(third); // Chi
+
+//Array Destructuring 2
+let [raindrops, whiskers, ...aFewOfMyFavoriteThings] = [
+  "Raindrops on roses",
+  "whiskers on kittens",
+  "Bright copper kettles",
+  "warm woolen mittens",
+  "Brown paper packages tied up with strings"
+]
+
+console.log(raindrops); // Raindrops on roses
+console.log(whiskers); // Whiskers on kittens
+console.log(aFewOfMyFavoriteThings); // The rest of the array
+
+//Array Destructuring 3
+let numbers = [10, 20, 30];
+[numbers[1], numbers[2]] = [numbers[2], numbers[1]]
+
+console.log(numbers) // 10, 30, 20
+
+//ES2015 Object Destructuring
+const obj = {
+  numbers: {
+    a: 1,
+    b: 2
+  }
+};
+const {a, b} = obj.numbers;
+
+//ES2015 Array Swap
+const arr = [1, 2];
+[arr[0], arr[1]] = [arr[1], arr[0]];
+
+//raceResults()
+const results = ([first, second, third, ...rest]) => ({first, second, third, rest});
