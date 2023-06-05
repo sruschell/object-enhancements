cc629e9f54968677f1ccd234c32e4382071dcd3c
renee <s.n.ruschell@gmail.com>
2023-06-05 15:33:20 +0800

    add maps and sets

--- objEnhance.js
+++ objEnhance.js
@@ -179,3 +179,40 @@ const arr = [1, 2];
 
 //raceResults()
 const results = ([first, second, third, ...rest]) => ({first, second, third, rest});
+
+//MAPS AND SETS
+//Q1. What does this code return:
+new Set([1,1,2,2,3,4]); //[1,2,3,4]
+
+//Q2. What does this code return:
+[...new Set("referee")].join(""); //ref
+
+//Q3. What does Map m look like after code is run?
+let m = new Map();
+m.set([1,2,3], true);
+m.set([1,2,3], false);
+//[1,2,3] false
+
+//hasDuplicate
+const hasDuplicate = arr => new Set(arr).size !== arr.length;
+
+//vowelCount
+function isVowel(char){
+  return "aeiou".includes(char); 
+}
+
+function vowelCount(str){
+  const vowelMap = new Map();
+  for(let char of str){
+    let lowerCaseChar = char.toLowerCase()
+    if(isVowel(lowerCaseChar)){
+      if(vowelMap.has(lowerCaseChar)){
+        vowelMap.set(lowerCaseChar, vowelMap.get(lowerCaseChar) + 1);
+      
+      }else{
+        vowelMap.set(lowerCaseChar, 1);
+      }
+    }
+  }
+  return vowelMap;
+}
